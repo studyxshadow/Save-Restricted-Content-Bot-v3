861580bd5c7498451a0232236b236d54a40f3d19
studyxshadow <studyxshadow@gmail.com>
2026-07-29 10:33:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,8 +20,8 @@ YTUB_COOKIES = """
 """
 
 # ─── BOT / DATABASE CONFIG ──────────────────────────────────────────────────────
-API_ID       = os.getenv("API_ID", "")
-API_HASH     = os.getenv("API_HASH", "")
+API_ID       = os.getenv("API_ID", "28935416")
+API_HASH     = os.getenv("API_HASH", "e18c05697d95edfe39d8957f6e110308")
 BOT_TOKEN    = os.getenv("BOT_TOKEN", "")
 MONGO_DB     = os.getenv("MONGO_DB", "")
 DB_NAME      = os.getenv("DB_NAME", "telegram_downloader")
